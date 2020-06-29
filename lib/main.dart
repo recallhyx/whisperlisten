dafd1a66227cbe1e6e31d4a344554e51cd95007f
recallhyx <quce.hu@qq.com>
2020-06-29 22:31:24 +0800

    feat: add entry page

--- lib/main.dart
+++ lib/main.dart
@@ -9,109 +9,214 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '卿听',
+      themeMode: ThemeMode.dark,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+        brightness: Brightness.dark,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    '卿听',
+                    style: TextStyle(
+                      fontSize: 80,
+                    ),
+                  ),
+                  Text(
+                    '--独乐乐不如众乐乐',
+                    style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Container(
+              height: 120,
+              width: 160,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  InkWell(
+                    child: Container(
+                        // width: 120,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(3)),
+                          gradient: LinearGradient(
+                            begin: Alignment(-1, -1),
+                            end: Alignment(1.0, 1),
+                            colors: <Color>[
+                              Color(0xB2FE6B8B),
+                              Color(0xFFFF8E53),
+                            ],
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              '创建房间',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    onTap: () {},
+                  ),
+                  SearchInput(),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class SearchInput extends StatefulWidget {
+  _SearchInputState createState() => new _SearchInputState();
+}
+
+class _SearchInputState extends State<SearchInput>
+    with SingleTickerProviderStateMixin {
+  bool isExpand = false;
+  bool isTyping = false;
+  AnimationController _controller;
+  Animation<double> animation;
+
+  initState() {
+    super.initState();
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 400), vsync: this);
+    animation = CurvedAnimation(parent: _controller, curve: Curves.easeInOut)
+      ..addListener(() {
+        setState(() {});
+      });
+  }
+
+  Future<void> startAnimation() async {
+    // 调用 AnimationController 的 forward 方法启动动画
+    await _controller.forward();
+  }
+
+  Future<void> reverseAnimation() async {
+    // 调用 AnimationController 的 forward 方法启动动画
+    await _controller.reverse();
+  }
+
+  // AnimateInput animateInput = new AnimateInput();
+  Widget build(BuildContext context) {
+    return Container(
+        height: 50,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            AnimatedInput(animation: animation),
+            Container(
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                  borderRadius: isExpand
+                      ? BorderRadius.only(
+                          topRight: Radius.circular(3),
+                          bottomRight: Radius.circular(3))
+                      : BorderRadius.all(Radius.circular(3)),
+                  gradient: LinearGradient(
+                    begin: Alignment(-1, -1),
+                    end: Alignment(1.0, 1),
+                    colors: <Color>[
+                      Color(0xB22196F3),
+                      Color(0xFF21CBF3),
+                    ],
+                  )),
+              child: InkWell(
+                child: Icon(Icons.search),
+                onTap: _handleClick,
+              ),
+            )
+          ],
+        ));
+  }
+
+  void _handleClick() {
+    if (!isExpand) {
+      setState(() {
+        isExpand = true;
+      });
+      _controller.forward();
+      return;
+    }
+    setState(() {
+      isExpand = false;
+    });
+    FocusScope.of(context).unfocus();
+    _controller.reverse();
+  }
+
+  dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
+
+class AnimatedInput extends AnimatedWidget {
+  AnimatedInput({Key key, Animation<double> animation})
+      : super(key: key, listenable: animation);
+  static final _sizeTween = Tween<double>(begin: 0, end: 110);
+  Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
+    return Container(
+      width: _sizeTween.evaluate(animation),
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(3), bottomLeft: Radius.circular(3))),
+        child: TextField(
+          decoration: InputDecoration(
+            hintText: "请输入房间号",
+            hintStyle: TextStyle(color: Colors.grey),
+            focusColor: Colors.blue,
+            contentPadding: const EdgeInsets.symmetric(vertical: 12),
+            border: InputBorder.none,
+            // borderRadius: BorderRadius.only(topLeft: Radius.circular(3),bottomLeft: Radius.circular(3))
+          ),
+          style: TextStyle(
+            fontSize: 16,
+            color: Color(0xB22196F3),
+          ),
+          textAlign: TextAlign.center,
+          textAlignVertical: TextAlignVertical.center,
+        ),
+      ),
     );
   }
 }
